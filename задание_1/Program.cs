c74d60bf1d687ff576b00065a1ad9472afa7d512
Artem <Art-80919900@mail.ru>
2023-04-23 17:22:51 +0500

    дз

---
+++ задание_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    return (int.Parse(Console.ReadLine())); 
+}
+int a = EnterData("Введите число A: ");
+int b = EnterData("Введите число B: ");
+int i = 0;
+i = Convert.ToInt32(Math.Pow(a,b));
+Console.Write($"{a}, {b}, -> {i}"); 
+
---
+++ задание_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    return (int.Parse(Console.ReadLine())); 
+}
+int a = EnterData("Введите число A ");
+int result = 0;
+int num = a;
+int b = a;
+while(num > 0)
+{
+    num = b % 10;
+    result = result + num;
+    b = b / 10;
+} 
+Console.WriteLine($"{a} -> {result}");
